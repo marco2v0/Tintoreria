03a5d34dc9212b448fb4b6dc4f712f12de635b97
marco <marco2.v0@gmail.com>
2018-06-21 19:59:04 -0500

    Modificaciones a Catálogo de notas

--- tintoreria/notas/serializers.py
+++ tintoreria/notas/serializers.py
@@ -12,6 +12,39 @@ class DetalleSerializer (ModelSerializer):
 class NotaSerializer(ModelSerializer):
 	detalle = DetalleSerializer(many=True);
 
+	def create (self, validated_data):
+		print(validated_data)
+		#detalle_nota = validated_data.pop('detalle')
+		#print(detalle_nota)
+		nota = Nota()
+		detalle_nota = Detalle()
+		nota.cantidad = validated_data['cantidad']
+		nota.persona_servicio = validated_data['persona_servicio']
+		nota.observaciones = validated_data['observaciones']
+		nota.status = validated_data['status']
+		nota.cliente = validated_data['cliente']
+		nota.fecha_termino = validated_data['fecha_termino']
+		nota.fecha_entrega = validated_data['fecha_entrega']
+		nota.descuento = validated_data['descuento']
+		nota.servicio = validated_data['servicio']
+		#nota.id = validated_data['id']
+
+		detalle_nota = validated_data['detalle']
+
+		nota.save()
+
+		detalle_nvo = Detalle()
+
+		for detalle in detalle_nota:
+			detalle_nvo.nota = nota
+			detalle_nvo.articulo = detalle['articulo']
+			detalle_nvo.cantidad = detalle['cantidad']
+			detalle_nvo.partida = detalle['partida']
+			detalle_nvo.save()
+			print(detalle_nvo.cantidad)
+
+		return validated_data
+
 	class Meta:
 		model  = Nota
 		fields = ('id',
--- tintoreria/notas/views.py
+++ tintoreria/notas/views.py
@@ -5,10 +5,10 @@ from __future__ import unicode_literals
 import json
 from rest_framework.views import APIView
 from django.http import HttpResponse
-from tintoreria.notas.models import Nota, Status
+from tintoreria.notas.models import Nota, Status, Detalle
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from tintoreria.notas.serializers import NotaSerializer, StatusSerializer
+from tintoreria.notas.serializers import NotaSerializer, StatusSerializer, DetalleSerializer
 from rest_framework import generics
 
 # Create your views here.
@@ -50,3 +50,19 @@ class StatusList(generics.ListCreateAPIView):
 class StatusDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
+
+class DetalleAPI(APIView):
+	serializer = DetalleSerializer
+
+	def get(self,request,format=None):
+		lista = Detalle.objects.all()
+		response = self.serializer(lista,many=True)
+		return HttpResponse(json.dumps(response.data),content_type='application/json')
+
+class DetalleList(generics.ListCreateAPIView):
+    queryset = Detalle.objects.all()
+    serializer_class = DetalleSerializer
+
+class DetalleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Detalle.objects.all()
+    serializer_class = DetalleSerializer
